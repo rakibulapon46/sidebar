50640338cf78cfc6b24ade53de27d3eb8995dec4
rakibulapon46 <134295199+rakibulapon46@users.noreply.github.com>
2024-02-03 00:48:02 +0600

    all-done

---
+++ js/index.js
@@ -0,0 +1,18 @@
+const toggleBtn = document.querySelector(".sidebar_toggle");
+const closeBtn = document.querySelector(".sidebar_close");
+const sideBar = document.querySelector(".sidebar_area");
+
+toggleBtn.addEventListener("click", () => {
+  /*
+    if (sideBar.classList.contains('show_sidebar')) {
+        sideBar.classList.remove('show_sidebar');
+    } else {
+        sideBar.classList.add('show_sidebar');
+    }
+    */
+  sideBar.classList.toggle("show_sidebar");
+});
+
+closeBtn.addEventListener("click", () => {
+    sideBar.classList.remove('show_sidebar');
+})
